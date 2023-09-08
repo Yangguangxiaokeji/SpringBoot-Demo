6e585eb2c91a52a16de16635439a992ff1984570
wangxin <wx1063140310@163.com>
2023-09-08 14:00:01 +0800

    feat-PageResult分页查询行为参数化

--- common/src/main/java/com/foogui/common/model/page/PageResult.java
+++ common/src/main/java/com/foogui/common/model/page/PageResult.java
@@ -1,5 +1,7 @@
 package com.foogui.common.model.page;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -7,6 +9,7 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.function.Supplier;
 
 
 /**
@@ -19,16 +22,30 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class PageResult<T> implements Serializable {
+public class PageResult<R> implements Serializable {
 
     private static final long serialVersionUID = 6845513814060581181L;
 
-    private List<T> data;
+    private List<R> data;
 
     private Long total;
 
     private Integer currentPage;
 
     private Integer pageSize;
+
+    public <R> PageResult<R> getPageResult(int pageNum, int pageSize, Supplier<List<? extends R>> supplier){
+
+        PageHelper.startPage(pageNum, pageSize);
+
+        PageInfo<R> pageInfo = new PageInfo<>(supplier.get());
+
+        return PageResult.<R>builder()
+                .data(pageInfo.getList())
+                .total(pageInfo.getTotal())
+                .currentPage(pageNum)
+                .pageSize(pageSize)
+                .build();
+    }
 
 }
