f3ae92c3c311039bfa4ef6697d837fa4095ba86b
wangxin_zxb <wangxin213@faw.com.cn>
2023-10-21 23:14:04 +0800

    feat-openfeign初始化

---
+++ boot-openfeign/src/main/java/com/foogui/bootopenfeign/BootOpenfeignApplication.java
@@ -0,0 +1,13 @@
+package com.foogui.bootopenfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootOpenfeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootOpenfeignApplication.class, args);
+    }
+
+}
